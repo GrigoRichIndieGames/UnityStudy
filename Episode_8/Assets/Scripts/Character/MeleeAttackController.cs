fe6e2b16e443864b0448d13e799e90757d136fee
GrigoRichIndieGames <59909506+GrigoRichIndieGames@users.noreply.github.com>
2020-05-03 15:36:21 +1000

    03052020

---
+++ Episode_8/Assets/Scripts/Character/MeleeAttackController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class MeleeAttackController : MonoBehaviour
+{
+    public Transform AttackPoint;
+    public LayerMask DamageableLayerMask;
+    public float Damage;
+    public float AttackRange;
+    public float TimeBtwAttack;
+
+    private Animator _animator;
+    private float _timer;
+
+    private void Start()
+    {
+        GetReferences();
+    }
+
+    private void Update()
+    {
+        Attack();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(AttackPoint.position, AttackRange);
+    }
+
+    private void Attack()
+    {
+        if (_timer <= 0)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                _animator.SetTrigger("Attack");
+
+                Collider2D[] enemies = Physics2D.OverlapCircleAll(AttackPoint.position, AttackRange, DamageableLayerMask);
+
+                if (enemies.Length != 0)
+                {
+                    for (int i = 0; i < enemies.Length; i++)
+                    {
+                        enemies[i].GetComponent<DamageableObject>().TakeDamage(Damage);
+                    }
+                }
+
+                _timer = TimeBtwAttack;
+            }
+        }
+        else
+        {
+            _timer -= Time.deltaTime;
+        }
+    }
+
+    private void GetReferences()
+    {
+        _animator = GetComponent<Animator>();
+    }
+}
---
+++ Episode_8/Assets/Scripts/Enemy/EnemyMoveController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class EnemyMoveController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
+    private Vector2 _horizontalVelocity;
+    private Vector3 _leftFlip = new Vector3(0, 180, 0);
+    private float _signPreviosFrame;
+    private float _signCurrentFrame;
+
+    private void Start()
+    {
+        GetReferences();
+    }
+
+    private void Update()
+    {
+        Flip();
+    }
+
+    public void Move(float speed)
+    {
+        _horizontalVelocity.Set(speed, _rigidbody.velocity.y);
+        _rigidbody.velocity = _horizontalVelocity;
+    }
+
+    private void Flip()
+    {
+        _signCurrentFrame = _rigidbody.velocity.x == 0 ? _signPreviosFrame : Mathf.Sign(_rigidbody.velocity.x);
+
+        if (_signCurrentFrame != _signPreviosFrame)
+        {
+            transform.rotation = Quaternion.Euler(_rigidbody.velocity.x < 0 ? _leftFlip : Vector3.zero);
+        }
+        _signPreviosFrame = _signCurrentFrame;
+    }
+
+    private void GetReferences()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+}
