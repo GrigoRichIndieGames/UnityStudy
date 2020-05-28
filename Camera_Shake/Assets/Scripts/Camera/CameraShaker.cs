36e34eba583f60feeaa460e19bc06ffbaee7d104
GrigoRichIndieGames <59909506+GrigoRichIndieGames@users.noreply.github.com>
2020-05-28 11:58:46 +1000

    28052020

---
+++ Camera_Shake/Assets/Scripts/Camera/CameraShaker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraShaker : MonoBehaviour
+{
+    public int QtOfAnimations = 5;
+
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void Shake()
+    {
+        int index = Random.Range(0, QtOfAnimations + 1);
+
+        _animator.SetTrigger($"Shake{index}");
+    }
+}
