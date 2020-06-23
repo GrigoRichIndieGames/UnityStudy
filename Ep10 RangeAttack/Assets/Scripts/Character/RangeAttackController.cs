5b44113d5cc6ed783e1a9638c655da32cd3df0ee
GrigoRichIndieGames <59909506+GrigoRichIndieGames@users.noreply.github.com>
2020-06-23 20:51:17 +1000

    23062020

---
+++ Ep10 RangeAttack/Assets/Scripts/Character/RangeAttackController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class RangeAttackController : MonoBehaviour
+{
+    public GameObject ArrowPrefab;
+    public Transform RangeWeaponPosition;
+    public Transform TargetPoint;
+    public float TimeBtwShot;
+
+    private Arrow _currentArrow;
+    private Vector3 _direction;
+    private float _rotationZ;
+    private float _timer;
+    private bool _hasArrow;
+
+
+    private void Start()
+    {
+        CreateArrow();
+    }
+
+    private void Update()
+    {
+        Aim();
+        CreateArrow();
+        Shoot();
+    }
+
+    public void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1") && _hasArrow)
+        {
+            _currentArrow.Shoot();
+            _currentArrow.transform.parent = null;
+            _hasArrow = false;
+        }
+    }
+
+    private void Aim()
+    {
+        if (Input.GetButton("Fire2"))
+        {
+            _direction = TargetPoint.position - RangeWeaponPosition.position;
+            _rotationZ = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
+            RangeWeaponPosition.rotation = Quaternion.Euler(0, 0, _rotationZ);
+        }
+    }
+
+    private void CreateArrow()
+    {
+        if (!_hasArrow && _timer <= 0)
+        {
+            _currentArrow = Instantiate(ArrowPrefab, RangeWeaponPosition.GetChild(0)).GetComponent<Arrow>();
+            _hasArrow = true;
+            _timer = TimeBtwShot;
+        }
+        else if (!_hasArrow)
+        {
+            _timer -= Time.deltaTime;
+        }
+    }
+}
---
+++ Ep10 RangeAttack/Assets/Scripts/Weapon/Arrow.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public float LifeTime;
+    public float Damage;
+    public float Force;
+
+    private Rigidbody2D _rigidbody;
+    private bool _isShot;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Rotate();
+    }
+
+    public void Shoot()
+    {
+        _rigidbody.simulated = true;
+        _rigidbody.AddForce(transform.right * Force, ForceMode2D.Impulse);
+        Destroy(gameObject, LifeTime);
+        _isShot = true;
+    }
+
+    private void Rotate()
+    {
+        if (_isShot)
+        {
+            var direction = _rigidbody.velocity;
+            var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+    }
+}
